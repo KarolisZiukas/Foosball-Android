d0095388b44f981431b58c1da2aa17b51d2d7c80
Karolis <karolis.ziu@gmail.com>
2017-10-29 14:22:26 +0200

    added data basic data fetch from api

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,19 +1,20 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
-using System;
-using Android.Media;
+using System.Json;
+using System.Net;
+
 namespace Foosball_Android
 {
     [Activity(Label = "Foosball_Android", MainLauncher = true)]
     public class MainActivity : Activity
     {
-
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
             loadFragment();
+           
         }
 
         private void loadFragment()
