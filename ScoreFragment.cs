c44d173eff6eb723833de675e0ed8fff4088ec86
Karolis <karolis.ziu@gmail.com>
2017-11-19 18:32:57 +0200

    added covariance

--- ScoreFragment.cs
+++ ScoreFragment.cs
@@ -6,18 +6,23 @@ using Android.Widget;
 using System.Json;
 using System.Net;
 using System;
-using System.IO;
-using Android.Media;
 using System.Threading.Tasks;
+using Android.Media;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Foosball_Android
 {
+
+
     [Activity(Label = "ScoreFragment")]
-    public class ScoreFragment : Fragment
+    public class ScoreFragment<T> : Fragment
     {
         MediaPlayer _mediaPlayer;
         TextView redTeamTextView;
         TextView blueTeamTextView;
+        
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
 
@@ -26,22 +31,18 @@ namespace Foosball_Android
             View view = inflater.Inflate(Resource.Layout.ScoreFragment, null);
             redTeamTextView = view.FindViewById<TextView>(Resource.Id.red_team_text_view);
             blueTeamTextView = view.FindViewById<TextView>(Resource.Id.blue_team_text_view);
-            //redTeamTextView.Click += delegate
-            //{
-            //    _mediaPlayer = MediaPlayer.Create(Application.Context, Resource.Raw.red_team_scored);
-            //    _mediaPlayer.Start();
-            //};
+
+            //ToDo Karolis: await/async
             redTeamTextView.Click += async (sender, e) =>
             {
-                string url = "http://private-c9b9e-foosball2.apiary-mock.com/foosballApp/questions";
+
+                string url = "http://172.26.21.75:5000/api/values";
                 JsonValue json = await Fetchdata(url);
 
             };
 
-
-
             blueTeamTextView.Click += delegate
-            {
+            {          
                 _mediaPlayer = MediaPlayer.Create(Application.Context, Resource.Raw.blue_team_scored);
                 _mediaPlayer.Start();
             };
@@ -53,7 +54,6 @@ namespace Foosball_Android
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
             request.ContentType = "application/json";
             request.Method = "GET";
-
             using (WebResponse response = await request.GetResponseAsync())
             {
 
@@ -61,7 +61,7 @@ namespace Foosball_Android
 
                 {
                     JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
-                    Toast.MakeText(this.Activity,jsonDoc.ToString(), ToastLength.Short).Show();
+                    Toast.MakeText(this.Activity, jsonDoc.ToString(), ToastLength.Short).Show();
                     return jsonDoc;
                 }
 
---
+++ Utility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Foosball_Android
+{
+    class Utility
+    {
+        public int CalCulateTotalScore(IEnumerable<TotalScore> lstHomeAppliance)
+        {
+            var total = 0;
+            foreach (var p in lstHomeAppliance)
+            {
+                total += p.Score;
+            }
+            return total;
+        }
+    }
+}
